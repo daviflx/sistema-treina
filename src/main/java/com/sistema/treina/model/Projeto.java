16a6efcd8329337b25fda3ff8ceca5c3fb452fa5
Davi Félix <davi10felixs@gmail.com>
2026-08-04 11:12:03 -0300

    Segundo commit

--- src/main/java/com/sistema/treina/model/Projeto.java
+++ src/main/java/com/sistema/treina/model/Projeto.java
@@ -1,5 +1,21 @@
 package com.sistema.treina.model;
 
+import com.sistema.treina.enums.StatusProjeto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "projeto")
 public class Projeto {
-    
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String descricao;
+    StatusProjeto status;
 }
--- src/main/java/com/sistema/treina/model/Usuario.java
+++ src/main/java/com/sistema/treina/model/Usuario.java
@@ -1,5 +1,20 @@
 package com.sistema.treina.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    String senha;
+    String dataCadastro;
 }
