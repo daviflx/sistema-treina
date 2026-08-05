2d6434248b7f2d0bbda3f660f11d81741533b827
Davi <davi10felixs@gmail.com>
2026-08-05 15:20:43 -0300

    Especificando tabelas

--- src/main/java/com/sistema/treina/model/Usuario.java
+++ src/main/java/com/sistema/treina/model/Usuario.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "usuarios")
@@ -16,19 +17,20 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nome", nullable = false)
+    @Column(name = "nome", nullable = false, length = 100)
     @NotNull
     private String nome;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, length = 50)
     @NotNull
     private String email;
 
-    @Column(name = "senha", nullable = false)
+    @Column(name = "senha", nullable = false, length = 20)
     @NotNull
+    @Size(min = 8, message = "A senha deve ter no mínimo 8 caracteres")
     private String senha;
 
-    @Column(name = "data_cadastro", nullable = false)
+    @Column(name = "data_cadastro", nullable = false, length = 10)
     @NotNull
     private String dataCadastro;
 }
